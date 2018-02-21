be4993afead8d9da44f402d4673fb09cca4f41b0
eliemugenzi <eliemugenzi@gmail.com>
2018-02-21 15:43:59 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+  $('.parallax').parallax();
+  $('.carousel.carousel-slider').carousel({fullWidth:true});
+
+  let options={
+    scrollOffset:100
+  }
+  $('.scrollspy').scrollSpy(options);
+  $('.slider').slider();
+  $(".button-collapse").sideNav();
+})
